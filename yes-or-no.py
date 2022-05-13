f9dc8a36dc4ef3a1c20070a7c3eecabef6dead5c
shirentheprogramer <105465923+shirentheprogramer@users.noreply.github.com>
2022-05-13 16:07:27 -0500

    yes-or-no

---
+++ yes-or-no.py
@@ -0,0 +1,25 @@
+import time
+#you like jazz?
+jazz = input("do you like jazz (yes/no/what is jazz)")
+if jazz == "yes":
+    print("cool me too")
+if jazz == "no":
+    print(" oh well")
+if jazz == "what is jazz":
+    print("go watch the bee movie")
+#transision     
+time.sleep(2)
+nex = input("ready for the next one? (yes/no)")
+if nex == "yes":
+    print("good for you")
+if nex == "no":
+    print(" to bad")
+#band music
+time.sleep(2)
+band = input("do you like band music (yes/no/meh)")
+if band == "yes":
+    print(" i love band music too ")
+if band == "no":
+    print(" hmmmmmmmmmm fine whatever")
+if band == "meh":
+    print("wow sooooooooooooooooooo enthusiastic")
