62337370146e14c20e960e7e6fd2ad28e66c1135
shirentheprogramer <105465923+shirentheprogramer@users.noreply.github.com>
2022-05-13 16:32:59 -0500

    shiren

---
+++ shiren.py
@@ -0,0 +1,22 @@
+a=5
+b=5
+
+for i in range(10):
+    print("hi")
+    
+    while(2==2):
+        print("hi")
+
+
+
+
+
+
+list=[]
+list.append("S")
+list.append("h")
+list.append("i")
+list.append("r")
+list.append("e")
+list.append("n")
+print (list)
