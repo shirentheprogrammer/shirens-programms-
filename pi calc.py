43a2594ee5a1820d8596e86973e42aaea9ad78e2
shirentheprogramer <105465923+shirentheprogramer@users.noreply.github.com>
2022-05-31 19:29:13 -0500

    pi

---
+++ pi calc.py
@@ -0,0 +1,6 @@
+radius= float(input("Enter radius of circle: "))
+area = 3.14*radius**2
+circumference = 2*3.14*radius
+
+print("Area of circle:",area)
+print("circumference of circle:",circumference)
