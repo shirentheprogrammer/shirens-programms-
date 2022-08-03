71f7ff2845e637ca291c6220be0f332949b2c5e0
shirentheprogramer <105465923+shirentheprogramer@users.noreply.github.com>
2022-08-03 15:37:02 -0500

    ds

--- talk.py
+++ talk.py
@@ -1,15 +1,27 @@
+from pickle import STOP
 import time
-name=input("what is your name?")
+name=input("what is your name?:")
 time.sleep(1)
-print("well hi there " +name+".")
+print(f"well hi there {name}")
 time.sleep(1)
-food=input("so "+name+" what is your favorite food?")
+food=input(f"so {name} what is your favorite food?:")
 time.sleep(1)
-whatfood=input("wow i have never tried " +food+" can you discribe it?")
+whatfood=input(f"wow i have never tried {food} can you discribe it?:")
 time.sleep(1)
 print("wow that sounds good")
 time.sleep(1)
-age=input("so how old are you " +name)
+age=input(f"so how old are you {name}?:")
 time.sleep(1)
-print("ahhh so you are " +age+" i am 13")
+print(f"ahhh so you are {age} i am... you know i really dont know!")
 time.sleep(1)
+next=input(f"so {name} do you want to continue?:")
+if next=="yes":
+    print("as you wish!!!!")
+elif next=="no":
+    print(f"alright then have a great day {name}!")
+    
+else:
+    print("im going to assume you want to continue")
+
+
+
