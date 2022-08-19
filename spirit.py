93e8a1e210f65c35f9daa13464e6b9beba19a453
shirentheprogramer <105465923+shirentheprogramer@users.noreply.github.com>
2022-08-18 19:08:24 -0500

    h

---
+++ spirit.py
@@ -0,0 +1,56 @@
+
+
+
+import time 
+
+
+
+# A new world
+print(" you wake to flowers falling around you, a majestic fox looking at you walks closer!")
+time.sleep(6)
+print(" unknown fox: you seem scared dont worry ill be your gide through the spirit relm, come now we cant wait around forever!")
+time.sleep(6)
+trust=input(" you: hm i wonder if i should trust this fox? (yes/no):")
+
+if trust=="yes":
+    print(" you follow the fox deep into the enchanted woods")
+    time.sleep(6)
+    print(" Ahri: my name is Ahri by the way... whats yours?")
+    time.sleep(6)
+    print(" you think long and hard but cant seem to remember... anything.")
+    time.sleep(6)
+    print(" Ahri: silly me i forgot spirits dont remember anything after they die!")
+    print (" Ahri: all thanks to thresh...")
+    time.sleep(6)
+    print(" after the mention of this thresh Ahri walked silently for a long time")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if trust=="no":
+    print (" the fox leaves you and thresh harvests your essense and you are traped in his lantern for all of your afterlife!!!!") 
+
+else: 
+    print(" invaled ")
+    
+    
+    
+    
+
+
+
+
+
+
