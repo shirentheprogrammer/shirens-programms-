df969ee6985f066c8177371babc96426f88225fd
shirentheprogramer <105465923+shirentheprogramer@users.noreply.github.com>
2022-08-19 13:43:32 -0500

    update

--- spirit.py
+++ spirit.py
@@ -3,9 +3,13 @@ import time
 
 trustQuestions = ["You follow the fox deep into the enchanted woods",
                   "Ahri: my name is Ahri by the way... whats yours?",
-                  "you think long and hard but cant seem to remember... anything. Ahri: all thanks to thresh...",
-                  "after the mention of this thresh Ahri walked silently for a long time"
-                    ]
+                  "you think long and hard but cant seem to remember... anything."," Ahri: silly me i forgot when spirit come here they have no memory if there life as a mortal",
+                  "Ahri: all thanks to thresh...",
+                    "after the mention of this thresh Ahri walked silently for a long time"
+                        ]
+
+
+exposed = ["suddenly Ahri begins to tremble "]
 
 
 
@@ -28,16 +32,19 @@ if trust=="yes":
     print(trustQuestions[3])
     time.sleep(6)
     print(trustQuestions[4])
+    time.sleep(6)
+    print(trustQuestions[5])
+
     
+q1=input("should you ask her who thresh is?(yes/no):")
+if q1=="yes":
+    print(" Ahri: well if you must know... Thresh is an ambitious and restless spirit of the Shadow Isles. Once the custodian of countless arcane secrets, he was undone by a power greater than life or death, and now sustains himself by tormenting and breaking others with slow, excruciating inventiveness. His victims suffer far beyond their brief mortal coil as Thresh wreaks agony upon their souls, imprisoning them in his unholy lantern to torture for all eternity.")
+time.sleep(20)
     
-    q1=input("should you ask her who thresh is?(yes/no):")
-    if q1=="yes":
-        print(" Ahri: well if you must know... Thresh is an ambitious and restless spirit of the Shadow Isles. Once the custodian of countless arcane secrets, he was undone by a power greater than life or death, and now sustains himself by tormenting and breaking others with slow, excruciating inventiveness. His victims suffer far beyond their brief mortal coil as Thresh wreaks agony upon their souls, imprisoning them in his unholy lantern to torture for all eternity.")
-        time.sleep(20)
-    
-    if q1=="no":
-        print("you dont ask and keep walking")
-        print(" Ahri: by the way um stick with me if you dont want thresh well to... you know... torture you for all eternity!")
+if q1=="no":
+    print("you dont ask and keep walking")
+    print(" Ahri: by the way um stick with me if you dont want thresh well to... you know... torture you for all eternity!")
+        
 
 
 
